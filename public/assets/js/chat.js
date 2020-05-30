a447df5b63d176cf521d2002371e8187f3b60af3
Emir Kabal <kabalemir@gmail.com>
2020-05-31 02:51:22 +0300

    Some bugs fixed.

--- public/assets/js/chat.js
+++ public/assets/js/chat.js
@@ -4,13 +4,19 @@ let userId = $('#userId').text();
 let message = $('#message');
 
 socket.on('message', (data) => {
-    $('#messages').append(`<div class="message">${md.render(data.message).replace(/<h[0-9]>/g, '<p>').replace(/<\/h1[0-9]>/g, '</p>').replace(/<blockquote>/g, '').replace(/<\/blockquote>/g, '')}</div>`);
-    if (data.id === userId) $("#messages").find("p:last").addClass('sendMessage')
+    $('#messages').append(`<div class="message">${md.render(data.message)
+        .replace(/<h[0-9]>/g, '<p>').replace(/<\/h1[0-9]>/g, '</p>')
+        .replace(/<blockquote>|<\/blockquote>/g, '')
+        .replace(/<hr>/g, '')
+        .replace(/<ul>|<\/ul>/g, '')
+        .replace(/<li>|<\/li>/g, '')
+        .replace(/<img/g, '<img style="max-width: 8%; max-height: 8%;" ')}</div>`);
+    if (data.id === userId) $("#messages").find("div:last").addClass('sendMessage')
     $('html').scrollTop($('html').prop('scrollHeight') + 100)
     $('#typing').hide();
 });
 
-socket.on('typing', (data) => {
+socket.on('typing', () => {
     $('#typing').show();
 });
 
@@ -22,10 +28,21 @@ socket.on('online', (data) => {
 $('#sendMessage').click(function () {
     if (message.val().replace(/ /g, '') === '') return;
     socket.emit('message', {id: userId, message: message.val()})
+    let old = message.attr("placeholder");
+    message.attr('disabled', 'disabled');
+    message.removeAttr("placeholder")
+    message.attr("placeholder", "Waiting...");
+    setTimeout(() => {
+        message.removeAttr("placeholder");
+        message.attr("placeholder", old);
+        message.removeAttr('disabled');
+        message.focus();
+    }, 1250);
     message.val('');
 })
 
 $(document).on('keypress',function(e) {
+    message.focus();
     if(e.which !== 13) return;
     $('#sendMessage').click();
     setTimeout(() => {
